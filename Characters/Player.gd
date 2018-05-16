bc3a627d58a1ac8b355b9ab404c93a68edfed00a
Grant <TheGrai@users.noreply.github.com>
2018-05-15 22:36:23 -0400

    Added Friction
    + Player now has friction

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -10,26 +10,27 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	direction = Vector2()
+	
+	var frictionx = false
+	var frictiony = false 
 
 	if Input.is_action_pressed("move_left"):
 		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 	elif Input.is_action_pressed("move_right"):
 		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 	else:
-		motion.x = 0
+		frictionx = true
+	
 	if Input.is_action_pressed("move_up"):
 		motion.y = max(motion.y-ACCELERATION, -MAX_SPEED)
 	elif Input.is_action_pressed("move_down"):
 		motion.y = min(motion.y+ACCELERATION, MAX_SPEED)
 	else:
-		motion.y = 0
-
+		frictiony = true
 	
-
-#	if direction != Vector2():
-#		speed = MAX_SPEED
-#	else:
-#		speed = 0
-
+	if frictionx == true:
+		motion.x = lerp(motion.x, 0, 0.5)
+	if frictiony == true:
+		motion.y = lerp(motion.y, 0, 0.5)
 
 	move_and_slide(motion)
