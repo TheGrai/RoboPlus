94c84b4b71718b69ed2839757daa8aa9eae05a6a
Grant <TheGrai@users.noreply.github.com>
2018-05-15 22:18:37 -0400

    Added Character Acceleration
    + Added Acceleration to player movement

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -3,29 +3,33 @@ extends KinematicBody2D
 var direction = Vector2()
 
 var speed = 20
-const MAX_SPEED = 400
-
-var velocity = Vector2()
+const ACCELERATION = 20
+const MAX_SPEED = 200
+var motion = Vector2()
 
 
 func _physics_process(delta):
 	direction = Vector2()
 
 	if Input.is_action_pressed("move_left"):
-		direction.x = -1
+		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 	elif Input.is_action_pressed("move_right"):
-		direction.x = 1
-
-	if Input.is_action_pressed("move_up"):
-		direction.y = -1
-	elif Input.is_action_pressed("move_down"):
-		direction.y = 1
-
-	if direction != Vector2():
-		speed = MAX_SPEED
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 	else:
-		speed = 0
+		motion.x = 0
+	if Input.is_action_pressed("move_up"):
+		motion.y = max(motion.y-ACCELERATION, -MAX_SPEED)
+	elif Input.is_action_pressed("move_down"):
+		motion.y = min(motion.y+ACCELERATION, MAX_SPEED)
+	else:
+		motion.y = 0
 
-	velocity = speed * direction
+	
 
-	move_and_slide(velocity)
+#	if direction != Vector2():
+#		speed = MAX_SPEED
+#	else:
+#		speed = 0
+
+
+	move_and_slide(motion)
